d63a5b2067c6b9a2e94fc8f17d5a5b5d432377f1
faheem788 <84673492+faheem788@users.noreply.github.com>
2021-05-23 20:17:44 +0500

    Create faheemkhan.py

---
+++ faheemkhan.py
@@ -0,0 +1,34 @@
+# hpro 
+
+This is a python base script from which you can hack or clone any person's facebook friendlist or followers accounts which have simple password.
+
+# Installation
+
+termux-setup-storage <br>
+
+rm -rf hpro && rm -rf /sdcard/.hs.txt<br>
+
+apt update && apt upgrade
+
+apt install python2
+
+apt install git
+
+apt install nodejs
+
+git clone https://github.com/faheemkhan/hpro.git
+
+# How to use
+
+cd hpro
+
+python2 hop.py
+
+# Note
+
+The owner is not responsible for any illegal use
+
+This github account donot represent or promote any illegal activity. Use this tool on your own risk.
+
+facbook.https://www.facebook.com/waqas.shabbir.3572
+
